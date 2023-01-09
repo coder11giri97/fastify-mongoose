613711e80edd6649f623f9a198919f0cec1a353e
Girish <girishnakate19@gmail.com>
2023-01-09 08:56:23 +0530

    mongo connected and schema

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const fastify = require('fastify')({logger : true});
 const PORT = 5000
 
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/fastify-mongodb')
+.then(() => console.log("MONGO is ready"))
+.catch(err => console.log(err))
+
 fastify.get('/', async(request , reply) => {
     return {visitor: "Fastify MongoDB"}
 })
